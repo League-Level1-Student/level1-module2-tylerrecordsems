ad0acc8c8458737d0da0f2fc0afa45e232f5a481
root <root@110f67da4b35>
2020-12-14 21:21:47 +0000

    hi

---
+++ src/_01_sea_creature/UNDA_DA_SEA.java
@@ -0,0 +1,18 @@
+package _01_sea_creature;
+
+public class UNDA_DA_SEA {
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+SeaCreature sponge = new SeaCreature("bob");
+SeaCreature squid = new SeaCreature("ward");
+SeaCreature patrick = new SeaCreature("star");
+sponge.eat();
+//sponge.laugh();
+patrick.getName();
+squid.getName();
+patrick.eat();
+squid.eat();
+//patrick.laugh();
+//squid.laugh();
+}
+}
---
+++ src/_02_smurf/smurf_Run.java
@@ -0,0 +1,17 @@
+package _02_smurf;
+
+public class smurf_Run {
+public static void main(String[] args) {
+	Smurf mma = new Smurf("Rowdy roody Piper");
+	Smurf PAPA = new Smurf ("Smurft=papa");
+Smurf Smurfette = new Smurf ("I can fly");
+	mma.eat();
+	mma.getName();
+	PAPA.getName();
+	PAPA.getHatColor();
+	PAPA.isGirlOrBoy();
+	Smurfette.getName();
+	Smurfette.getHatColor();
+	Smurfette.isGirlOrBoy();
+}
+}
