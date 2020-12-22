9ed11ef6af84e26bdc645a86a7a859dfac49da1d
root <root@7db03b39f82e>
2020-12-22 21:31:49 +0000

    YAY!

---
+++ src/GI_Joe.java
@@ -0,0 +1,63 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import org.omg.Messaging.SyncScopeHelper;
+
+public class GI_Joe {
+	
+	JButton leftButton = new JButton();
+	JButton centerButton = new JButton();
+	JButton rightButton = new JButton();
+	JButton oneButton = new JButton();
+	JButton twoButton = new JButton();
+	JButton threeButton = new JButton();
+	JButton fourButton = new JButton();
+	JButton fiveButton = new JButton();
+	JButton sixButton = new JButton();
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	public void character(){
+//	String Name = JOptionPane.showInputDialog( null, "Witch GI_JOE would you like to be?");
+	frame.add(panel);
+	
+frame.setVisible(true);
+
+leftButton.setText("Duke!");
+centerButton.setText("SnakeEyes!");
+oneButton.setText("Flint!");
+twoButton.setText("Lady Jay!");
+threeButton.setText("Sgt. Slaughter");
+rightButton.setText("Zartan!");
+fourButton.setText("Spirit!");
+fiveButton.setText("RoadBlock!");
+sixButton.setText("Shipreck!");
+
+
+frame.setBounds(450, 0, 500, 100);
+panel.add(leftButton);
+panel.add(rightButton);
+panel.add(centerButton);
+panel.add(oneButton);
+panel.add(twoButton);
+panel.add(threeButton);
+panel.add(fourButton);
+panel.add(fiveButton);
+panel.add(sixButton);
+frame.setTitle("Which GI Joe would you like to be? YO JOE!!!");
+//frame.pack();
+	leftButton.addActionListener((ActionListener) this);
+	centerButton.addActionListener((ActionListener) this);
+	rightButton.addActionListener((ActionListener) this);
+	
+	}
+	public void actionPerformed(ActionEvent arg0) {
+		JButton buttonPressed = (JButton) arg0.getSource();
+	
+	}
+}
---
+++ src/GI_Joe_Runner.java
@@ -0,0 +1,13 @@
+
+
+public class GI_Joe_Runner {
+public static void main(String[] args) {
+	new GI_Joe().character();
+	GI_Joe GIjoe = new GI_Joe();
+	GIjoe.character();
+}
+}
+
+	
+		
+	
