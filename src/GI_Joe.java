51d53e9449ef5afd204f909005b52a04b5923a95
root <root@7db03b39f82e>
2020-12-22 23:39:43 +0000

    hi

--- src/GI_Joe.java
+++ src/GI_Joe.java
@@ -9,7 +9,7 @@ import javax.swing.JPanel;
 import org.omg.Messaging.SyncScopeHelper;
 
 public class GI_Joe {
-	
+String x;
 	JButton leftButton = new JButton();
 	JButton centerButton = new JButton();
 	JButton rightButton = new JButton();
@@ -24,7 +24,8 @@ public class GI_Joe {
 	JPanel panel = new JPanel();
 	public void character(){
 //	String Name = JOptionPane.showInputDialog( null, "Witch GI_JOE would you like to be?");
-	frame.add(panel);
+
+		frame.add(panel);
 	
 frame.setVisible(true);
 
@@ -56,8 +57,28 @@ frame.setTitle("Which GI Joe would you like to be? YO JOE!!!");
 	rightButton.addActionListener((ActionListener) this);
 	
 	}
+	public void chosen() {
+		JOptionPane.showMessageDialog(null, "You have chosen " + x);
+	}
 	public void actionPerformed(ActionEvent arg0) {
 		JButton buttonPressed = (JButton) arg0.getSource();
-	
+		if (buttonPressed == leftButton) {
+			x.equals("Duke!");
+			chosen();
+			frame.setVisible(false);
+		}if (buttonPressed == centerButton) {
+			x.equals("SnakeEyes!");
+		}if (buttonPressed == oneButton) {
+			x.equals("Flint!");
+		}if (buttonPressed == twoButton) {
+			x.equals("Lady Jay");
+		}if (buttonPressed == threeButton) {
+			x.equals("Sgt. Slaughter");
+		}if (buttonPressed == rightButton) {
+			x.equals("Zartan");
+		}if (buttonPressed == fourButton) {
+			x.equals("Spirit");
+		}
+		
 	}
 }
--- src/GI_Joe_Runner.java
+++ src/GI_Joe_Runner.java
@@ -1,10 +1,20 @@
+import java.awt.event.ActionEvent;
 
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 public class GI_Joe_Runner {
 public static void main(String[] args) {
+//	int x;
 	new GI_Joe().character();
 	GI_Joe GIjoe = new GI_Joe();
 	GIjoe.character();
+	GIjoe.chosen();
+}
+public void actionPerformed(ActionEvent arg0) {
+	JButton buttonPressed = (JButton) arg0.getSource();
+//	int x;
+//JOptionPane.showMessageDialog(null, "You have chosen " + x);
 }
 }
 
