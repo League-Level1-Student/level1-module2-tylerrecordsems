9b9c1cbc11fc5696c908dde050d27c732885637d
root <root@e5cef2656312>
2021-01-04 21:13:07 +0000

    hi

--- src/_06_tea_party/TeaParty.java
+++ src/_06_tea_party/TeaParty.java
@@ -3,42 +3,20 @@ package _06_tea_party;
 import org.junit.Assert;
 
 public class TeaParty {
-    public void welcome(String name, boolean isWoman, boolean isKnighted) {
-    	
-    	import static org.junit.Assert.assertEquals;
-    	import org.junit.Test;
+	public String welcome(String name, boolean isWoman, boolean isKnighted) {
 
-    	class TeaPartyTest {
-    		/**
-    		 * Jane Austen is a woman, so say “Hello Ms. Austen”. 
-    		 * George Orwell is a man, so say “Hello Mr. Orwell”. 
-    		 * Isa ac Newton was knighted, so say "Hello Sir Isaac Newton".
-    		 **/  
-
-    		@Test
-    		public void testIsWomanNotKnighted() {
-    			TeaParty teaParty = new TeaParty();
-    			assertEquals("Hello Ms. Austen", teaParty.welcome("Austen", true, false));
-    		}
-
-    		@Test
-    		public void testIsManNotKnighted() {
-    			TeaParty teaParty = new TeaParty();
-    			assertEquals("Hello Mr. Orwell", teaParty.welcome("Orwell", false, false));
-    		}
-
-    		@Test
-    		public void testIsManKnighted() {
-    			TeaParty teaParty = new TeaParty();
-    			assertEquals("Hello Sir Isaac Newton", teaParty.welcome("Isaac Newton", false, true));
-    		}
-    		
-    		@Test
-    		public void testIsWomanKnighted() {
-    			TeaParty teaParty = new TeaParty();
-    			assertEquals("Hello Lady Laura", teaParty.welcome("Laura", true, true));
-    		}
-
-    	}
-    }
+		if (isKnighted && isWoman) {
+			return "Hello Lady " + name;
+		}
+if (!isWoman && !isKnighted) {
+	return "Hello Mr. " + name;
+}
+if (!isWoman && isKnighted) {
+	return "Hello Sir " + name;
+}
+if (isWoman && !isKnighted) {
+	return "Hello Ms. " + name;
+}
+		return null;
+	}
 }
---
+++ src/_06_tea_party/TeaPartyTest.java
@@ -0,0 +1,38 @@
+package _06_tea_party;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TeaPartyTest {
+	/**
+	 * Jane Austen is a woman, so say “Hello Ms. Austen”. 
+	 * George Orwell is a man, so say “Hello Mr. Orwell”. 
+	 * Isaac Newton was knighted, so say "Hello Sir Isaac Newton".
+	 **/
+
+	@Test
+	public void testIsWomanNotKnighted() {
+		TeaParty teaParty = new TeaParty();
+		assertEquals("Hello Ms. Austen", teaParty.welcome("Austen", true, false));
+	}
+
+	@Test
+	public void testIsManNotKnighted() {
+		TeaParty teaParty = new TeaParty();
+		assertEquals("Hello Mr. Orwell", teaParty.welcome("Orwell", false, false));
+	
+	}
+
+	@Test
+	public void testIsManKnighted() {
+		TeaParty teaParty = new TeaParty();
+		assertEquals("Hello Sir Isaac Newton", teaParty.welcome("Isaac Newton", false, true));
+	}
+	
+	@Test
+	public void testIsWomanKnighted() {
+		TeaParty teaParty = new TeaParty();
+		assertEquals("Hello Lady Laura", teaParty.welcome("Laura", true, true));
+	}
+
+}
