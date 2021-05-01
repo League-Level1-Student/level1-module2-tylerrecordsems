7b7cd182dffaa4bd20f4e326f68c0d4dc425e4d6
student <student@ip-172-31-61-230.us-west-2.compute.internal>
2021-05-01 16:23:46 -0700

    GI_JOE!

--- src/GI_Joe.java
+++ src/GI_Joe.java
@@ -8,82 +8,83 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import org.omg.Messaging.SyncScopeHelper;
 
-public class GI_Joe {
-String x;
-	JButton leftButton = new JButton();
-	JButton centerButton = new JButton();
-	JButton rightButton = new JButton();
-	JButton oneButton = new JButton();
-	JButton twoButton = new JButton();
-	JButton threeButton = new JButton();
-	JButton fourButton = new JButton();
-	JButton fiveButton = new JButton();
-	JButton sixButton = new JButton();
+public class GI_Joe implements ActionListener {
+String chosen;
+	JButton Duke = new JButton();
+	JButton SnakeEyes = new JButton();
+	JButton Zartan = new JButton();
+	JButton Flint = new JButton();
+	JButton SgtSlaughter = new JButton();
+//	JButton fourButton = new JButton();
+//	JButton fiveButton = new JButton();
+	//JButton sixButton = new JButton();
 
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	public void character(){
-//	String Name = JOptionPane.showInputDialog( null, "Witch GI_JOE would you like to be?");
+//	String Name = JOptionPane.showInputDialog( null, "Which GI_JOE would you like to be?");
 
 		frame.add(panel);
 	
 frame.setVisible(true);
 
-leftButton.setText("Duke!");
-centerButton.setText("SnakeEyes!");
-oneButton.setText("Flint!");
-twoButton.setText("Lady Jay!");
-threeButton.setText("Sgt. Slaughter");
-rightButton.setText("Zartan!");
-fourButton.setText("Spirit!");
-fiveButton.setText("RoadBlock!");
-sixButton.setText("Shipreck!");
+Duke.setText("Duke!");
+SnakeEyes.setText("SnakeEyes!");
+Flint.setText("Flint!");
+SgtSlaughter.setText("Sgt. Slaughter!");
+Zartan.setText("Zartan!");
+//fourButton.setText("Spirit!");
+//fiveButton.setText("RoadBlock!");
+//sixButton.setText("Shipreck!");
 
 
 frame.setBounds(450, 0, 500, 100);
-panel.add(leftButton);
-panel.add(rightButton);
-panel.add(centerButton);
-panel.add(oneButton);
-panel.add(twoButton);
-panel.add(threeButton);
-panel.add(fourButton);
-panel.add(fiveButton);
-panel.add(sixButton);
+panel.add(Duke);
+panel.add(Zartan);
+panel.add(SnakeEyes);
+panel.add(Flint);
+
+panel.add(SgtSlaughter);
+//panel.add(fourButton);
+//panel.add(fiveButton);
+//panel.add(sixButton);
 frame.setTitle("Which GI Joe would you like to be? YO JOE!!!");
 //frame.pack();
-	leftButton.addActionListener((ActionListener) this);
-	centerButton.addActionListener((ActionListener) this);
-	rightButton.addActionListener((ActionListener) this);
+Duke.addActionListener(this);
+SnakeEyes.addActionListener(this);
+Flint.addActionListener(this);
 	
+SgtSlaughter.addActionListener(this);
+	Zartan.addActionListener(this);
 	}
 	public void chosen() {
-		JOptionPane.showMessageDialog(null, "You have chosen " + x);
+		JOptionPane.showMessageDialog(null, "You have chosen " + chosen);
 	}
+	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		JButton buttonPressed = (JButton) arg0.getSource();
-		if (buttonPressed == leftButton) {
-			x.equals("Duke!");
-			chosen();
-			frame.setVisible(false);
-		}if (buttonPressed == centerButton) {
-			x.equals("SnakeEyes!");
-		}if (buttonPressed == oneButton) {
-			x.equals("Flint!");
-		}if (buttonPressed == twoButton) {
-			x.equals("Lady Jay");
-		}if (buttonPressed == threeButton) {
-			x.equals("Sgt. Slaughter");
-		}if (buttonPressed == rightButton) {
-			x.equals("Zartan");
-		}if (buttonPressed == fourButton) {
-			x.equals("Spirit");
-		}
-	//	void reset() {
-	//		frame.dispose();
-	//		frame = new JFrame();
-	//		panel = new Panel();
-	//		frame.add(panel);
-	//	}
+		// TODO Auto-generated method stub
+	if(arg0.getSource()==Duke) {
+		chosen.equals("Duke!");
+	}if(arg0.getSource()==SgtSlaughter) {
+		chosen.equals("Sgt. Slaughter!");
+	}if(arg0.getSource()==Zartan) {
+		chosen.equals("Zartan!");
+	}if(arg0.getSource()==SnakeEyes) {
+		chosen.equals("SnakeEyes!");
+	}if(arg0.getSource()==Flint) {
+		chosen.equals("Flint!");
 	}
+		frame.dispose();
+		frame = new JFrame();
+		panel = new JPanel();
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.pack();
+	}
+	
+
+
+		
+	
 }
+
